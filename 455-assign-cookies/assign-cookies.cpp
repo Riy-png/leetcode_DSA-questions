568abc0eb82ff7072a9cd7a94576820d469be4eb
Riy-png <riyasingh567784@gmail.com>
2026-06-15 11:53:04 +0530

    Time: 7 ms (70.68%) | Memory: 44.9 MB (46.97%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int greed=0;
+        int sweets=0;
+
+        while(sweets <s.size() && greed<g.size()){
+            if(g[greed] <= s[sweets]){
+                greed++;
+            }
+            sweets++;
+
+        }
+        return greed;
+    }
+};        
