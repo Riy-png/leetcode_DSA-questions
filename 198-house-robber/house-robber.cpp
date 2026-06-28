caf5cdae156a1e3c3132b1fc0e5c109d7e6b1819
Riy-png <riyasingh567784@gmail.com>
2026-06-28 21:10:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (69.17%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -2,22 +2,18 @@ class Solution {
 public:
     vector<int> dp;
 
-    int rob(vector<int>& nums, int n) {
-        if (n == 0) return 0;
-        if (n == 1) return nums[0];
-
-        if (dp[n] != -1)
-            return dp[n];
-
-        return dp[n] = max(
-            rob(nums, n - 1),
-            nums[n - 1] + rob(nums, n - 2)
-        );
+    int robHelper(vector<int>& nums, int n) {
+      if(n==0) return 0;
+      if(n==1) return nums[0];
+      if(dp[n]!=-1) return dp[n];
+      return dp[n]= max
+      (robHelper(nums,n-1),
+      nums[n-1] + robHelper(nums,n-2)
+      );
     }
-
-    int rob(vector<int>& nums) {
-        int n = nums.size();
-        dp.resize(n + 1, -1);
-        return rob(nums, n);
-    }
+    int rob(vector<int>& nums){
+        int n =nums.size();
+        dp.assign(n+1,-1);
+        return robHelper(nums,n);
+    }  
 };
