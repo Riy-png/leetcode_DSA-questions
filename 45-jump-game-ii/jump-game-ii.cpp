665c9ddc39b54f9a04cf41e2514eaabf71fa0432
Riy-png <riyasingh567784@gmail.com>
2026-07-02 21:05:44 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (29.27%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n =nums.size();
+        int jumps=0;
+        int maxReach=0;
+        int currentEnd=0;
+        for(int i=0;i<n-1;i++){
+             maxReach= max(maxReach, i+nums[i]);
+             if(i==currentEnd){
+              jumps++;
+              currentEnd = maxReach;
+             }
+        }
+            
+        return jumps;
+    }
+};
