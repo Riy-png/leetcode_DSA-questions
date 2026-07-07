536303a7c99c39f2a0e45afe3b28a5140e3c30c0
Riy-png <riyasingh567784@gmail.com>
2026-07-07 21:10:39 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (99.68%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-     vector<vector<int>> solve(int target, vector<int>& candidates,int idx) {
-           if (target == 0)
-            return {{}} ;
+     void solve(int target, vector<int>& candidates,int ind ,vector<int>& ds,vector<vector<int>> &ans ) {
+        if(ind== candidates.size()){
+            if(target==0){ 
+              return ans.push_back(ds);
+            } 
+           return ;
+        }      
 
-        if (idx == candidates.size() || target < 0)
-            return {} ;
-
-        vector<vector<int>> ans;
-
-        // Pick current element
-       auto take= solve(target - candidates[idx],candidates, idx );
-        for (auto &v : take) {
-            v.push_back(candidates[idx]);
-            ans.push_back(v);
+        if(candidates[ind]<=target){
+        ds.push_back(candidates[ind]);
+        solve(target-candidates[ind],candidates,ind,ds,ans);
+        ds.pop_back();  
         }
-
-        // Skip all duplicates while not picking
-        int next = idx + 1;
-
-        auto notTake=solve(target,candidates, next);
-        ans.insert(ans.end(), notTake.begin(), notTake.end());
-
-        return ans;   
+        solve(target,candidates,ind+1,ds,ans);
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-         sort(candidates.begin(), candidates.end());
-        return solve(target,candidates,0);
+        vector<vector<int>> ans;
+        vector<int> ds;
+        solve(target,candidates,0,ds,ans);
+        return ans;
     }
 };
