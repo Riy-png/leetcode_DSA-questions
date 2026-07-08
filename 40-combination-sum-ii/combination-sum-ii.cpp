603178cf0540932fd1259f6fcacf2f78654f293c
Riy-png <riyasingh567784@gmail.com>
2026-07-08 19:48:06 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (63.62%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> solve(vector<int>& candidates, int target, int idx) {
+    void solve(vector<int>& candidates, int target, int ind , vector<int>& ds, vector<vector<int>> &ans) {
+           // base condition
+            if(target==0){ 
+              ans.push_back(ds);
+              return;
+            }  
 
-        if (target == 0)
-            return {{}};
-
-        if (idx == candidates.size() || target < 0)
-            return {};
-
-        vector<vector<int>> ans;
-
-        // Pick current element
-        auto take = solve(candidates, target - candidates[idx], idx + 1);
-        for (auto &v : take) {
-            v.push_back(candidates[idx]);
-            ans.push_back(v);
-        }
-
-        // Skip all duplicates while not picking
-        int next = idx + 1;
-        while (next < candidates.size() &&
-               candidates[next] == candidates[idx])
-            next++;
-
-        auto notTake = solve(candidates, target, next);
-        ans.insert(ans.end(), notTake.begin(), notTake.end());
-
-        return ans;
-    }
+            for(int i= ind;i<candidates.size();i++){
+             if(i>ind && candidates[i]==candidates[i-1]) 
+                continue;
+             if(candidates[i]>target)
+               break;
+               ds.push_back(candidates[i]);
+              solve(candidates,target-candidates[i],i+1,ds,ans);
+              ds.pop_back();
+            }
+           
+    }    
+        
+    
 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end());
-        return solve(candidates, target, 0);
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>> ans;
+        vector<int> ds;
+        solve(candidates,target,0,ds,ans);
+        return ans;
     }
 };
