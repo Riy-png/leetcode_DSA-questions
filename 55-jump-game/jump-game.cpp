211a55a6fc46a616f830907336591e1553d67d33
Riy-png <riyasingh567784@gmail.com>
2026-07-26 20:43:20 +0530

    Time: 3 ms (26.47%) | Memory: 52.4 MB (25.60%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n=nums.size();
-        int maxreach=0;
-        for(int i=0;i<n;i++){
-            if(i>maxreach){
+        int maxReach=0;
+        for(int i=0;i<nums.size();i++){
+            if(i>maxReach){
                 return false;
-            }    
-            maxreach=max(maxreach,i+nums[i]);
-
+            }
+            maxReach=max(maxReach,i+nums[i]);
         }
-        return true; 
+        return true;
     }
 };
